df2e511497c557cca26e1a458912094bb2af6d58
Ali <aliabduh.2020@gmail.com>
2022-03-13 11:24:12 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,25 @@
+
+<?php
+
+include( "header.php");
+
+?>
+
+    <div class="container">
+
+    <div class="row row-cols-1 m-auto w-50 mt-5 bg-light  p-5 card"  > 
+
+    <div class="h4 text-primary">  Total blance : </div>
+    <div class="h3 text-secondary"> 234$ </div>
+   
+
+    </div>
+
+</div>
+
+
+<?php
+
+include( "footer.php");
+
+?>
